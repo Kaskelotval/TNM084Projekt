a6a992fae230e514473d3250bc1fc2ee2e37bb98
Jens <jenskaske@hotmail.com>
2016-12-15 11:45:02 +0100

    Very ugly lights

--- shaders/fragment.js
+++ shaders/fragment.js
@@ -1,9 +1,35 @@
 var FragmentShader = `
         uniform vec2 u_resolution;
         uniform float u_time;
+        uniform vec3 u_light1Pos;
+        uniform vec3 u_light1Col;
+        uniform vec3 u_light2Col;
+        uniform vec3 u_light2Pos;
 
-        void main() {
-            vec2 st = gl_FragCoord.xy/u_resolution.xy;
-            gl_FragColor=vec4(abs(sin(u_time))*st.x,st.y,0.0,1.0);
+        varying vec3 vecNormal;
+        varying vec3 vecPos;
+        varying vec2 vUv;
+
+        void main(void) {
+
+		    vec3 st = vec3(vUv,1.0);
+
+        	//colors
+			vec3 basecolor = vec3(1.0, 1.0, 1.0);
+			float ambient = max(0.0, vecNormal.z);
+		    vec3 diffuseLight = vec3(0.3, 0.3, 0.3);
+
+
+            //LIGHTs
+		    vec3 lightDirection = normalize(u_light1Pos-vecPos);
+		    vec3 addedLights = clamp(dot(-lightDirection,vecNormal), 0.0, 1.0)*u_light1Col;
+
+		    vec3 lightDirection2 = normalize(u_light2Pos-vecPos);
+		    addedLights += clamp(dot(-lightDirection2,vecNormal), 0.0, 1.0)*u_light2Col;
+
+		    addedLights += diffuseLight;
+			//PHONG
+			float shininess = 5.0;
+            gl_FragColor=vec4(basecolor*addedLights,1.0);
         }
 `;   
